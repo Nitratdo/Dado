49edb37303b61ead093caa8a608c896780eeafba
Thomas_ <74280300+Nitratdo@users.noreply.github.com>
2022-08-06 02:02:58 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Dado
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random rnd = new Random();
+            for (int number = 0; number < 1; number++) ;
+            {
+                int x = rnd.Next(10);
+                Console.WriteLine("Chute um numero: ");
+                string Resposta = Console.ReadLine();
+
+                var y = Convert.ToInt16(Resposta);
+
+                if (y == x)
+                {
+                    Console.WriteLine("Parabéns Você acertou:", y);
+                }
+                else
+                {
+                    Console.WriteLine("Você Errou!!!");
+                }
+            }
+        }
+    }
+}
